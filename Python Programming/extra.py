1363b0ae57fb388667fe8d96c11a9fd75e0edb5f
anshulshongi <anshulshongi04@gmail.com>
2026-09-18 12:08:56 +0530

    litle update

---
+++ Python Programming/extra.py
@@ -0,0 +1,18 @@
+'''def add (a,b):
+    return a+b 
+print(add(4,5))
+
+x= int(input ("enter a number :"))
+if x/2==0 :
+    print("even")
+else:
+    print("odd")
+
+square = lambda x : x*x
+print (square(8))
+'''
+def square (n):
+    """square of an number"""
+    return n*n
+result= square(5)
+print(result)
